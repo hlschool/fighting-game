28db42d474c277baa29514ac5a44e534d4183d46
Carl Yoder <carlyodes1@gmail.com>
2019-04-30 12:40:37 -0400

    Add FPS Counter & Lock

--- fighting-game/Source.cpp
+++ fighting-game/Source.cpp
@@ -3,8 +3,14 @@ and may not be redistributed without written permission.*/
 
 //Using SDL and standard IO
 #include <SDL.h>
+#include <SDL_ttf.h>
 #include <stdio.h>
+#include <ctime>
+#include <iostream>
+#include <string>
+#include <chrono>
 #include "vector.h"
+using namespace std;
 
 //Screen dimension constants
 const int width = 640;
@@ -20,12 +26,19 @@ int main(int argc, char* args[])
 
 	SDL_Renderer* gRenderer = NULL;
 
-	const vector grav_acc = { 0, 0.00001 };
+	const SDL_Color white = { 255, 255, 255 };
+
+	const vector grav_acc = { 0, 0.1 };
 
 	vector pos = { width / 4, 0 };
 	vector vel = { 0, 0 };
 	vector acc = grav_acc;
 
+	int frames = 0;
+	int fps_count = 0;
+
+	const int fps_lock = 60;
+	const double spf = 1 / (double) fps_lock;
 
 	//Initialize SDL
 	if (SDL_Init(SDL_INIT_VIDEO) < 0)
@@ -55,29 +68,43 @@ int main(int argc, char* args[])
 			SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
 			SDL_RenderFillRect(gRenderer, &fillRect);
 
-
+			TTF_Init();
+			TTF_Font* Sans = TTF_OpenFont("OpenSans-Bold.ttf", 24);
+			if (Sans == NULL)
+			{
+				printf("Failed to load lazy font! SDL_ttf Error: %s\n", TTF_GetError());
+			}
 			SDL_Event evt;
 			bool programrunning = true;
 			while (programrunning)
 			{
+				auto start = chrono::high_resolution_clock::now();
 
 				while (SDL_PollEvent(&evt)) {
 					if (evt.type == SDL_QUIT)
 						programrunning = false;
 					else if (evt.type == SDL_MOUSEBUTTONDOWN) {
 						if (evt.button.button == SDL_BUTTON_LEFT) {
-							acc.x += 0.01;
+							acc.x += 0.1;
 						}
 						
 					}
 				}
 
+				SDL_SetRenderDrawColor(gRenderer, 90, 90, 90, 0xFF);
+				SDL_RenderFillRect(gRenderer, &backRect);
+
+				string fps_string = to_string(fps_count);
+				const char* fps_char = fps_string.c_str();
+				SDL_Surface* message_surf = TTF_RenderText_Solid(Sans, fps_char, white);
+				SDL_Texture* message = SDL_CreateTextureFromSurface(gRenderer, message_surf);
+				SDL_FreeSurface(message_surf);
+				SDL_Rect message_rect = { 0, 0, 100, 100 };
+				SDL_RenderCopy(gRenderer, message, NULL, &message_rect);
+
 				pos += vel;
 				vel += acc;
 				acc = grav_acc;
-
-				SDL_SetRenderDrawColor(gRenderer, 90, 90, 90, 0xFF);
-				SDL_RenderFillRect(gRenderer, &backRect);
 
 				fillRect.x = pos.x;
 				fillRect.y = pos.y;
@@ -86,6 +113,21 @@ int main(int argc, char* args[])
 
 				//Update the surface
 				SDL_RenderPresent(gRenderer);
+				
+				auto f_finish = chrono::high_resolution_clock::now();
+				chrono::nanoseconds f_elapsed = f_finish - start;
+				double f_elapsed_s = f_elapsed.count() * 1E-9;
+				cout << f_elapsed_s << endl;
+				if(f_elapsed_s < spf) {
+					SDL_Delay((spf - f_elapsed_s) * 1000);
+				}
+
+
+				auto finish = chrono::high_resolution_clock::now();
+				chrono::nanoseconds elapsed = finish - start;
+				double elapsed_s = elapsed.count() * 1E-9;
+				fps_count = 1 / elapsed_s;
+				//cout << fps_count << endl;
 				
 			}
 		}
